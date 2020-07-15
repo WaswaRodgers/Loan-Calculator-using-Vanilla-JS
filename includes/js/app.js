9ec22bbb70a890e43781890494d4b161c2a4da37
Waswa Rodgers <waswam1994@gmail.com>
2020-07-15 14:38:22 +0300

    Created computeResults to receive values from form

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,1 +1,9 @@
 document.getElementById("loan-form").addEventListener("submit", computeResults);
+
+function computeResults(e) {
+	const postAmount = document.getElementById("amount").value;
+	const postInterest = document.getElementById("interest").value;
+	const postYears = document.getElementById("years").value;
+
+	console.log(postAmount, postInterest, postYears)
+}
